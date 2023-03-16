31a891d47ab35a92c6f5fcc21226b461bdb6fe8f
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-03-16 04:23:31 +0100

    Removing GD from the package install

--- bin/install_packages.sh
+++ bin/install_packages.sh
@@ -6,7 +6,7 @@ add-apt-repository ppa:ondrej/php
 apt-get install sudo nginx php8.1-fpm php8.1 \
                 php8.1-mysqli php8.1-curl php8.1-memcached php8.1-memcache \
                 php8.1-zip php8.1-dom php8.1-mbstring -y php8.1-imagick \
-                php8.1-redis php8.1-bc php8.1-gd php8.1-intl php8.1-ssh2 \
+                php8.1-redis php8.1-bc php8.1-intl php8.1-ssh2 \
                 mariadb-client curl locales jq less -y
 
 rm -rf /var/lib/apt/lists/*

0f9aa190c59096274e68e2b094ac65359da6bc38
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2023-03-16 04:26:50 +0100

    Finalising the Ghostscript install
    The install script downloads, compiles and installs Ghostscript libraries, ImageMagick and the imagick PHP extension.

--- bin/install_ghostscript.sh
+++ bin/install_ghostscript.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install build-essential libtiff-dev -y
+sudo apt-get remove php8.1-imagick -y
+sudo apt-get install build-essential libtiff-dev zlib1g-dev libfontconfig-dev \
+                     libfreetype-dev libgvc6 libheif-dev liblcms2-dev \
+                     libopenjp2-7-dev liblqr-1-0-dev libopenexr-dev \
+                     libpango1.0-dev libraw-dev libwmf-dev libxml2-dev \
+                     libzip-dev libzstd-dev libdjvulibre-dev libraqm-dev \
+                     libwebp-dev libltdl-dev php8.1-dev -y
 
-curl -s -L "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs1000/ghostpdl-10.0.0.tar.gz" | tar -C /tmp -zx
+curl -s -L "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs1000/ghostscript-10.0.0.tar.gz" | tar -C /tmp -zx
+cd /tmp/ghostpdl-* && ./configure CFLAGS=-O3 --prefix=/usr && make -j $(nproc) so && make soinstall
+rm -rf /tmp/ghostpdl-*
+ldconfig
 
-cd /tmp/ghostpdl-10.0.0 && ./configure --prefix=/usr && make && make install
+curl -s -L https://imagemagick.org/archive/ImageMagick-6.9.12-81.tar.gz | tar -C /tmp -zx
+cd /tmp/ImageMagick-* && ./configure CFLAGS=-O3 --with-modules --with-gslib=yes && make -j $(nproc) && make install
+rm -rf /tmp/ImageMagick-*
+ldconfig
+
+curl -s -L https://pecl.php.net/get/imagick-3.7.0.tgz | tar -C /tmp -zx
+cd /tmp/imagick-* && phpize && ./configure CFLAGS=-O3 --with-imagick=/opt/local && make -j $(nproc) && make install
+rm -rf /tmp/imagick-*
+ldconfig
+
+echo "extension=imagick.so" > /etc/php/8.1/mods-available/imagick
+phpenmod imagick

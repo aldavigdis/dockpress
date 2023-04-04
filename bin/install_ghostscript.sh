dcf29f24adc7085111a9a83e227421b18ceb2b53
Alda Vigdis Skarphedinsdottir <191583+aldavigdis@users.noreply.github.com>
2023-04-04 00:00:19 +0000

    Updating Ubuntu version to 23.04 (#49)

--- bin/install_ghostscript.sh
+++ bin/install_ghostscript.sh
@@ -10,8 +10,8 @@ apt-get install build-essential libtiff-dev zlib1g-dev libfontconfig-dev \
                 libwebp-dev libltdl-dev php8.1-dev -y
 
 curl -s -L "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs1000/ghostscript-10.0.0.tar.gz" | tar -C /tmp -zx
-cd /tmp/ghost-* && ./configure CFLAGS=-O3 --prefix=/usr && make -j $(nproc) so && make soinstall
-rm -rf /tmp/ghost-*
+cd /tmp/ghostscript-* && ./configure CFLAGS=-O3 --prefix=/usr && make -j $(nproc) so && make soinstall
+rm -rf /tmp/ghostscript-*
 ldconfig
 
 curl -s -L "https://imagemagick.org/archive/releases/ImageMagick-6.9.12-82.tar.gz" | tar -C /tmp -zx
@@ -24,5 +24,5 @@ cd /tmp/imagick-* && phpize && ./configure CFLAGS=-O3 --with-imagick=/opt/local 
 rm -rf /tmp/imagick-*
 ldconfig
 
-echo "extension=imagick.so" > /etc/php/8.1/mods-available/imagick
+echo "extension=imagick.so" > /etc/php/8.1/mods-available/imagick.ini
 phpenmod imagick
